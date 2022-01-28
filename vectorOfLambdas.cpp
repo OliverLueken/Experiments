999ecf37d4f61b93833bc483ef0720ac11a2d072
Oliver Lüken <oliver.lueken@pm.me>
2022-01-28 15:48:22 +0100

    Adding vectorOfLambdas

---
+++ vectorOfLambdas.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <vector>
+
+auto lambda1 = [](){ std::cout << "1\n"; };
+auto lambda2 = [](){ std::cout << "2\n"; };
+auto lambda3 = [](){ std::cout << "3\n"; };
+
+int main(){
+    std::vector<void(*)()> lambdas{lambda1, lambda3, lambda2};
+
+    // for(const auto& lambda : lambdas){
+    //     lambda();
+    // }
+    //
+    std::ranges::for_each(lambdas, [](const auto& lambda){lambda();} );
+}
+
+/*
+program output:
+1
+3
+2
+*/
